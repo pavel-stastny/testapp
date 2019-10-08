66b13ee43d94c519690aeb3d7c1f371c5cd94329
pavel.stastny <pavel.stastny@accenture.com>
2019-10-08 17:19:29 +0200

    Threads

--- src/main/java/TestMain.java
+++ src/main/java/TestMain.java
@@ -1,7 +1,21 @@
 
 public class TestMain {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("Test openshift");
+
+        Thread.sleep(1000*10);
+
+        System.out.println("Test openshift after 10 seconds .. ");
+
+        Thread.sleep(1000*60);
+        System.out.println("Test openshift after minute .. ");
+
+
+        Thread.sleep(1000*60*5);
+        System.out.println("Test openshift after 5 minutes  .. ");
+
+        Thread.sleep(1000*60*15);
+        System.out.println("Test openshift after 15 minutes  .. ");
     }
 }
